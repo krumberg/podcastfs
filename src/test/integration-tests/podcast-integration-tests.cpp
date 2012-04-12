6d24fad28bcb85d55faa34991b554f30990ef573
Kristian Rumberg <kristianrumberg@gmail.com>
2012-04-12 08:22:45 +0200

    src/test: Added Google Test support and one integration test

---
+++ src/test/integration-tests/podcast-integration-tests.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+
+#include <stdio.h>
+
+extern "C" {
+#include <podcast/podcast.h>
+}
+
+TEST(podcast_integration_tests, from_file_read_name)
+{
+    const char* TESTRSS_FILE = "src/test/data/testrss.xml";
+
+    Podcast* podcast = podcast_new_from_file(TESTRSS_FILE);
+    ASSERT_TRUE(podcast != NULL);
+
+    ASSERT_FALSE(
+        strcmp(
+            "The Linux Action Show! MP3",
+            podcast_folder_name(podcast)
+        )
+    );
+
+    podcast_free(podcast);
+}
