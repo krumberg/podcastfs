69999ac6819c5deacf475c865c5247026225a519
Kristian Rumberg <kristianrumberg@gmail.com>
2012-04-14 10:45:10 +0200

    pcmount/main.c: Cleanup

--- src/apps/pcmount/main.c
+++ src/apps/pcmount/main.c
@@ -16,17 +16,6 @@
  *
  */
 
-// Hej
-
-#define FUSE_USE_VERSION 26
-
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <fuse.h>
-#include <glib.h>
-
 #include <pcmount/fuse.h>
 
 int main(int argc, char *argv[])
